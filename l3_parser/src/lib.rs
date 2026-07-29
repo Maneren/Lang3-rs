9b2aa8d5d3a2736a05ef90536be70c28848049f5
Maneren <maneren731@gmail.com>
2026-07-29 22:43:07 +0200

    replace handrolled parser with lalrpop

--- l3_parser/src/lib.rs
+++ l3_parser/src/lib.rs
@@ -1,12 +1,16 @@
-pub mod lexer;
-pub mod parser;
 pub mod token;
+pub mod lexer;
+pub mod loc_util;
+
+lalrpop_util::lalrpop_mod!(pub grammar);
 
 use l3_ast::Program;
-use parser::Parser;
+
 pub use token::Token;
 
 pub fn parse_program(source: &str, filename: &str) -> Result<Program, String> {
-    let mut parser = Parser::new(source, filename);
-    parser.parse_program()
+    let lexer = lexer::Lexer::new(source);
+    grammar::ProgramParser::new()
+        .parse(source, filename, lexer)
+        .map_err(|e| format!("Parse error: {}", e))
 }
---
+++ l3_parser/src/loc_util.rs
@@ -0,0 +1,55 @@
+use l3_location::*;
+
+pub fn byte_to_position(source: &str, byte: usize, filename: &str) -> Position {
+    let mut line: usize = 1;
+    let mut col: usize = 1;
+    for (i, c) in source.char_indices() {
+        if i >= byte {
+            break;
+        }
+        if c == '\n' {
+            line += 1;
+            col = 1;
+        } else {
+            col += 1;
+        }
+    }
+    Position::new(Some(filename.to_string()), line, col)
+}
+
+pub fn make_loc(begin: usize, end: usize, source: &str, filename: &str) -> Location {
+    Location::new(
+        byte_to_position(source, begin, filename),
+        byte_to_position(source, end, filename),
+    )
+}
+
+pub fn mk_id(name: &str, loc: Location) -> l3_ast::Identifier {
+    l3_ast::Identifier::new(name.to_string(), loc)
+}
+
+pub fn unescape_string(s: &str) -> String {
+    let mut result = String::with_capacity(s.len());
+    let mut chars = s.chars();
+    while let Some(c) = chars.next() {
+        if c == '\\' {
+            match chars.next() {
+                Some('n') => result.push('\n'),
+                Some('t') => result.push('\t'),
+                Some('\\') => result.push('\\'),
+                Some('"') => result.push('"'),
+                Some('x') => {
+                    let hex: String = chars.by_ref().take(2).collect();
+                    if let Ok(code) = u8::from_str_radix(&hex, 16) {
+                        result.push(code as char);
+                    }
+                }
+                Some(c) => result.push(c),
+                None => result.push('\\'),
+            }
+        } else {
+            result.push(c);
+        }
+    }
+    result
+}
